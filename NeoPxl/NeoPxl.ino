95c3bcf45408dbc508aa316cb28cca2cbc40c441
zefirpsaryanov <46773417+zefirpsaryanov@users.noreply.github.com>
2020-02-15 19:27:05 +0200

    15.02.2020

--- NeoPxl/NeoPxl.ino
+++ NeoPxl/NeoPxl.ino
@@ -16,7 +16,7 @@ void loop()
   //  strip.clear();
   //  colorWipe(strip.Color(255, 80, 0), 60);
   strip.clear();
-  color2ways(strip.Color(255, 80, 0), 60);
+  color2ways(strip.Color(255, 80, 0), 200);
 
 }
 
@@ -42,15 +42,14 @@ void colorWipeBack(int color, int wait)
 
 void color2ways(int color, int wait)
 {
-  for (int i = strip.numPixels() / 2 ; i < strip.numPixels(); i++)
+  int i, j;
+  for (i = 0, j = strip.numPixels() - 1; i <= (strip.numPixels() / 2 - 1)  && j >= strip.numPixels() / 2 ; i++, j--)
   {
-    for (int j = strip.numPixels() / 2 - 1; j >= 0; j--)
-    {
-      strip.setPixelColor(i, color);
-      strip.setPixelColor(j, color);
-      strip.show();
-      delay(wait);
-    }
+    strip.setPixelColor(i, color);
+    strip.setPixelColor(j, color);
+    strip.show();
+    delay(wait);
+
   }
 }
 
