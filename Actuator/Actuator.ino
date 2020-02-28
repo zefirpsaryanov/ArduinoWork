f21882b3b797cea8501b9acd8f52e440f7b7c4c3
zefirpsaryanov <46773417+zefirpsaryanov@users.noreply.github.com>
2020-02-28 19:28:32 +0200

    28.02.2020

--- Actuator/Actuator.ino
+++ Actuator/Actuator.ino
@@ -1,11 +1,17 @@
 // https://surtrtech.com/2018/01/27/step-by-step-on-how-to-use-the-l298n-dual-h-bridge-driver-with-arduino/
 // https://www.youtube.com/watch?v=ly5PToVtPfg&feature=emb_logo
 
-int in1 = 9;
+
+#define key1 5
+#define key2 6
+
+int in1 = 7;
 int in2 = 8;
 
 void setup()
 {
+  pinMode(key1, INPUT_PULLUP);// set pin as input
+  pinMode(key2, INPUT_PULLUP);// set pin as input
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
 }
@@ -28,7 +34,7 @@ void TurnMotorA2()
   digitalWrite(in2, HIGH);
 }
 
-void loop()
+void movement()
 {
   TurnMotorA1();
   delay(3000);
@@ -38,4 +44,15 @@ void loop()
   delay(3000);
   TurnOFFA();
   delay(2000);
+}
+
+void loop()
+{
+  int key1S = digitalRead(key1); // read if key1 is pressed
+  int key2S = digitalRead(key2); // read if key2 is pressed
+
+  if (!key1S) TurnMotorA1();
+  if (!key2S) TurnMotorA2();
+
+  delay(100);
 }
