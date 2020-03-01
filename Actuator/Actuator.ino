9bfb71f0859cbc35d00abacb18ace8ef232a7f3a
zefirpsaryanov <46773417+zefirpsaryanov@users.noreply.github.com>
2020-03-01 13:35:42 +0200

    01.03.2020

--- Actuator/Actuator.ino
+++ Actuator/Actuator.ino
@@ -2,29 +2,29 @@
 // https://www.youtube.com/watch?v=ly5PToVtPfg&feature=emb_logo
 
 
-#define key1 12
-#define key2 4
+#define key4 4  // DOWN
+#define key5 5  // UP
+#define key12 12  // UP
 
 int in1 = 7;
 int in2 = 8;
 
 void setup()
 {
-  pinMode(key1, INPUT_PULLUP);// set pin as input
-  pinMode(key2, INPUT_PULLUP);// set pin as input
+  pinMode(key4, INPUT_PULLUP);
+  pinMode(key5, INPUT_PULLUP);
+  pinMode(key12, INPUT_PULLUP);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
+  movement();
 }
 
 void TurnMotorA1()
 {
   digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-}
-
-void TurnOFFA()
-{
-  digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
 }
 
@@ -34,25 +34,37 @@ void TurnMotorA2()
   digitalWrite(in2, HIGH);
 }
 
+void TurnOFFA()
+{
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+}
+
 void movement()
 {
   TurnMotorA1();
-  delay(3000);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(30000);
   TurnOFFA();
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(2000);
   TurnMotorA2();
-  delay(3000);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(30000);
   TurnOFFA();
-  delay(2000);
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
 }
 
 void loop()
 {
-  int key1S = digitalRead(key1); // read if key1 is pressed
-  int key2S = digitalRead(key2); // read if key2 is pressed
+  int key4S = digitalRead(key4); // read if key1 is pressed
+  int key5S = digitalRead(key5); // read if key2 is pressed
+  int key12S = digitalRead(key12); // read if key2 is pressed
 
-  if (key1S) TurnMotorA1();
-  if (key2S) TurnMotorA2();
+  if (!key4S) TurnMotorA1();
+  if (!key5S) TurnMotorA2();
+  if (!key12S) TurnOFFA();
+  if (key4S && key5S) TurnOFFA();
 
   delay(100);
 }
