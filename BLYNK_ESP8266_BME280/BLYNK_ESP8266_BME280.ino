a8dea53259062a9e998089fe15043c974e3c42ff
zefirpsaryanov <46773417+zefirpsaryanov@users.noreply.github.com>
2020-03-14 16:46:18 +0200

    14.03.2020

--- BLYNK_ESP8266_BME280/BLYNK_ESP8266_BME280.ino
+++ BLYNK_ESP8266_BME280/BLYNK_ESP8266_BME280.ino
@@ -1,18 +1,13 @@
 #define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-#include <DHT.h>
+#include <Adafruit_BME280.h>
 
 char auth[] = "1a589190cd0e42caa727ea338ed16790";
 char ssid[] = "test";
 char pass[] = "1q2w3e4r";
 
-#define DHTPIN 2          // Digital pin 2
-
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22     // DHT 22, AM2302, AM2321
-
-DHT dht(DHTPIN, DHTTYPE);
+Adafruit_BME280 bme;
 float h = 0;
 float t = 0;
 
@@ -20,17 +15,17 @@ void setup()
 {
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass, IPAddress(192, 168, 10, 10), 8080);
-//  Blynk.begin(auth, ssid, pass, IPAddress(217, 174, 61, 217), 9444);
-  dht.begin();
+  //  Blynk.begin(auth, ssid, pass, IPAddress(217, 174, 61, 217), 9444);
+  bme.begin(0x76);
 }
 
 void loop()
 {
   Blynk.run();
-
-  h = dht.readHumidity();
-  t = dht.readTemperature();
-
+  t = bme.readTemperature();
+  h = bme.readHumidity();
+  Serial.println(h);
+  Serial.println(t);
   Blynk.virtualWrite(V15, h);  //V5 is for Humidity
   Blynk.virtualWrite(V16, t);  //V6 is for Temperature
 }
